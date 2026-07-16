a4daf8583f2957c36f6596585872483906ede215
Nikolai Salazar <nikolaijamesgs@gmail.com>
2026-07-16 15:01:30 -0400

    style: remove formatting file EOF blanks

--- src/formatting/index.ts
+++ src/formatting/index.ts
@@ -6,4 +6,3 @@ export {
   type ResumeFit,
   type ResumeFitMeasurements,
 } from './computeResumeFit'
-
--- src/tests/formatting.test.ts
+++ src/tests/formatting.test.ts
@@ -200,4 +200,3 @@ describe('computeResumeFit', () => {
     expect(constraints).toEqual(constraintsSnapshot)
   })
 })
-
